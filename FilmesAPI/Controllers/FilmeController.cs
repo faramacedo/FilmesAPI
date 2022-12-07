6bdcc168ca888f8e5a672c39aee4791cd332acb9
Fabio Raposo Macedo <faramacedo@gmail.com>
2022-12-07 10:08:50 -0300

    DataAnnotations, Get, Get por Id

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -2,22 +2,43 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net;
 
 namespace FilmesAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class FilmeController: ControllerBase
+    public class FilmeController : ControllerBase
     {
         private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
 
         [HttpPost]
         public void AdicionaFilme([FromBody] Filme filme)
         {
+            filme.Id = id++;
             filmes.Add(filme);
-            Console.WriteLine(filme.Titulo);
-            Console.WriteLine(filmes.Count);
+        }
+
+        [HttpGet]
+        public List<Filme> RecuperaFilme()
+        {
+            return filmes;
+        }
+
+        [HttpGet("{id}")]
+        public Filme RetornaFilme(int id)
+        {
+            //foreach (Filme filme in filmes)
+            //{
+            //    if (filme.Id == id)
+            //        return filme;
+            //}
+            //return null;
+
+            return filmes.FirstOrDefault<Filme>(filme => filme.Id == id);
         }
 
     }
--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,12 +1,18 @@
-﻿using System.Security.Permissions;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Permissions;
 
 namespace FilmesAPI.Models
 {
     public class Filme
     {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Campo Título é obrigatório.")]
         public string Titulo{ get; set; }
+        [Required(ErrorMessage = "Campo diretor é obrigatório.")]
         public string Diretor { get; set; }
+        [StringLength(40,ErrorMessage ="O campo Genero deve ter até 40 caracteres.")]
         public string  Genero { get; set; }
+        [Range(1,600, ErrorMessage ="A duração deve ser estar 1min e 10h.")]
         public int Duracao { get; set; }
     }
 }
