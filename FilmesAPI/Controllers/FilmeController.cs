b99a1df68554fbec7e0a5bc5b01f5af289f66fd4
Fabio Raposo Macedo <faramacedo@gmail.com>
2022-12-15 12:36:40 -0300

    Atribuição de retorno API + Configs banco MySQL

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -16,21 +16,21 @@ namespace FilmesAPI.Controllers
         private static int id = 1;
 
         [HttpPost]
-        public void AdicionaFilme([FromBody] Filme filme)
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
         {
             filme.Id = id++;
             filmes.Add(filme);
-            return Conte
+            return CreatedAtAction(nameof(RecuperaFilmePorId), new { Id = filme.Id }, filme);
         }
 
         [HttpGet]
-        public IEnumerable<Filme> RecuperaFilme()
+        public IActionResult RecuperaFilme()
         {
             return Ok(filmes);
         }
 
         [HttpGet("{id}")]
-        public IActionResult RetornaFilme(int id)
+        public IActionResult RecuperaFilmePorId(int id)
         {
             //foreach (Filme filme in filmes)
             //{
